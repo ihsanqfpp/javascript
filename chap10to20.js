9dd973e4c3d5176bf8713fdb1f7ad4b1ef1b6c69
ihsanqfpp <okzihsan956@gmail.com>
2024-08-04 02:43:39 -0800

    add the index

---
+++ chap10to20.js
@@ -0,0 +1,262 @@
+
+
+//10 if statements
+
+//let x=prompt("enter a number");
+//if (x==5){
+  //  alert("correct ");
+//}
+
+//let Name=prompt("enter your name ");
+//if (Name=="ihsan"){
+  //  alert("allowed");
+//}
+
+//let Name=prompt("enter your name ");
+//if (Name=="ihsan"){
+  //  console.log("allowed");
+//}
+
+//let Name="ihsan";
+//if (x==Name){
+  //  alert("good ");
+//}
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //Comparison operators > is greater than < is less than >= is greater than or equal to <= is less than or equal to !===  ===
+
+
+//let ticketNumber=prompt("enter your ticket number ");
+//if (ticketNumber !==122233){
+  //alert("bad luck");
+//}
+
+//let ticketNumber=prompt("enter your ticket number ");
+//if (ticketNumber ==122233){
+  //alert("good  luck");
+//}
+
+//let ticketNumber=prompt("enter your ticket number ");
+//if (ticketNumber !==122233){
+  //console.log("bad luck");
+//}
+
+
+//let num1=prompt("enter first number ");
+//let num2=prompt("enter second number");
+//if (num1 > num2 ){
+  //alert("num1 is greater than num2");
+//}
+
+//let num1=prompt("enter first number ");
+//let num2=prompt("enter second number");
+//if (num1 < num2 ){
+  //document.write("num1 is less than num2");
+//}
+
+//let num1=prompt("enter first number ");
+//let num2=prompt("enter second number");
+//if (num1 >=  num2 ){
+  //alert("num1 is greater or equal to num2");
+//}
+
+
+//let num1=prompt("enter first number ");
+//let num2=prompt("enter second number");
+//if (num1 <= num2 ){
+  //console.log("num1 is less or equal to  num2");
+//}
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+            //if...else and else if statements
+
+  //let Name=prompt("enter a name");
+  //if (Name=="ihsan"){
+    //alert("the name you entered is correct");
+  //}
+  //else{
+    //alert("the name you entered is wrong");
+  //}
+
+  //let num=prompt("enter a number ");
+  //if (num%2==0){
+    //console.log("the number is even : " +num);
+  //}
+  //else{
+    //console.log("the number is odd : " +num);
+  //}
+
+
+  //let num=prompt("enter a number ");
+  //if (num > 0){
+    //alert("the number is positive : " +num);
+  //}
+  //else{
+    //alert("the number is negative : " +num);
+  //}
+
+//let marks=prompt("enter your marks ");
+//if (marks >= 80){
+  //alert("grade A :" +marks);
+//}
+//else if(marks>=50){
+  //alert("grade B : " +marks);
+//}
+//else{
+  //alert("grade C : " +marks);
+//}
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4
+
+            //Testing sets of conditions
+        
+// let Name=prompt("enter your name ");
+// let age=prompt("enter your age ");
+// if (Name=="ihsan" && age==20){   AND CONDITION
+//   alert("hey ihsan! ");
+// }
+// else{
+//   alert("move on");
+// }
+
+
+
+// let Name=prompt("enter your name ");
+// let age=prompt("enter your age ");
+// if (Name=="ihsan" || age==20){   OR CONDITION
+//   console.log("hey ihsan! ");
+// }
+// else{
+//   console.log("move on");
+// }
+
+// let Name=prompt("enter your name ");
+// let age=prompt("enter your age ");
+// let res=prompt("enter your residence ")
+// if (Name=="ihsan" && age==20 || res=="hangu"){
+//   alert("hey ihsan! ");
+// }
+// else{
+//   alert("move on");
+// }
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+              //14     if statements nested
+  
+// let Name="ihsan";
+// let age=30;
+// let res="hangu";
+// if (Name=="ihsan"){
+//   if (age==30){
+//     alert(age+5);
+//   }
+//   else if (res=="hangu"){
+//     alert(Name + "ullah");
+//   }
+//   else {
+//     alert("going on");
+//   }
+// }
+// else {
+//   alert("good");
+// }
+
+
+// let Name=prompt("enter your name ");
+// let age=prompt("enter your age ");
+// let res=prompt("enter your city ");
+// if (Name=="musawir"){
+//   if (age==3){
+//     alert("cutie");
+//   }
+//   else if (res=="hangu"){
+//     alert("belong from hangu");
+// }
+// else{
+//   alert("this is not musawir");
+// }
+// }
+// else {
+//   alert("exit");
+// }
+
+//<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+                //15 Arrays
+
+// let city="hangu";
+// let city1="kohat";
+// let city2="orakzai";
+// let city3="peshawar";
+// let city4="kurram";
+// let city5="quetta";
+// let city6="islambad";
+
+// console.log("welcome to " +city);
+// console.log("pakistan capital is " +city6)
+
+
+
+// let cities=['hangu','kohat','orakzai','peshawar','kurram','islambad'];
+// console.log("wadi terah " +cities[2], " " +cities[1]);
+// console.log("the city of flowers is " +cities[3]);
+
+// let mixedArray=['ihsan ullah',20,'hangu'];
+// console.log(mixedArray[0], "his age is : " +mixedArray[1], "belong form :" +mixedArray[2])
+
+//**********************************************************************************************/
+
+
+                   //16 Arrays:    Adding and removing elements
+
+// let Names=[];
+
+// Names[0]="ihsan";
+// Names[1]="asmat";
+// Names[2]="naeem";
+// Names[3]="hafeez";
+// Names[4]="safeer";
+// Names[5]="musawir";
+
+// console.log( Names[0] + "ullah is the father of ", Names[5])
+
+// let Names=[];
+
+// Names[0]="ihsan";
+// Names[1]="asmat";
+// Names[2]="naeem";
+// Names[3]="hafeez";
+// Names[4]="safeer";
+// Names[5]="musawir";
+// console.log(Names)
+
+
+// let Names=[];
+// Names[0]="ihsan";
+// Names[1]="asmat";
+// Names[2]="naeem";
+// Names[3]="hafeez";
+// Names[4]="safeer";
+// Names[5]="musawir";
+// console.log(Names)
+// Names.pop();
+
+let Names=[];
+Names[0]="ihsan";
+Names[1]="asmat";
+Names[2]="naeem";
+Names[3]="hafeez";
+Names[4]="safeer";
+Names[5]="musawir";
+console.log(Names)
+Names.push("kalsoom");
+
+
+
+
+
+
+
